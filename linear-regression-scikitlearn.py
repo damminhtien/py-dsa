2bc1cd1be8e5df261de00197951a4160bb9dbd28
Dam Minh Tien <tiendamminh@viettel.com>
2018-06-29 15:30:09 +0700

    add linearregression and kmean

---
+++ linear-regression-scikitlearn.py
@@ -0,0 +1,30 @@
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+from sklearn import datasets, linear_model
+import numpy as np
+import matplotlib.pyplot as plt
+
+# height (cm)
+X = np.array([[147, 150, 153, 158, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
+# weight (kg)
+y = np.array([[ 49, 50, 51,  54, 58, 59, 60, 62, 63, 64, 66, 67, 68]]).T
+# Visualize data
+plt.plot(X, y, 'ro')
+plt.axis([140, 190, 45, 75])
+plt.xlabel('Height (cm)')
+plt.ylabel('Weight (kg)')
+plt.show()
+
+one = np.ones((X.shape[0], 1))
+Xbar = np.concatenate((one, X), axis = 1)
+A = np.dot(Xbar.T, Xbar)
+b = np.dot(Xbar.T, y)
+w = np.dot(np.linalg.pinv(A), b)
+print('w = ', w)
+# fit the model by Linear Regression
+regr = linear_model.LinearRegression(fit_intercept=False) # fit_intercept = False for calculating the bias
+regr.fit(Xbar, y)
+
+# Compare two results
+print( 'Solution found by scikit-learn  : ', regr.coef_ )
+print( 'Solution found by (5): ', w.T)
